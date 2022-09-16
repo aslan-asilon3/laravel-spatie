b52314a500d7a1a523c3f5ac6f8de730009f376b
Aslan Asilon <sulaslansetiawan3@gmail.com>
2022-09-16 15:14:41 +0700

    roles and permission

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,38 @@
+@extends('adminlte::page')
+
+@section('title', 'User Show')
+
+@section('content_header')
+    {{-- <h3>
+        User Show
+    </h3> --}}
+@stop
+
+@section('content')
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2> Show Product</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="{{ route('products.index') }}"> Back</a>
+        </div>
+    </div>
+</div>
+
+
+<div class="row">
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Name:</strong>
+            {{ $product->name }}
+        </div>
+    </div>
+    <div class="col-xs-12 col-sm-12 col-md-12">
+        <div class="form-group">
+            <strong>Details:</strong>
+            {{ $product->detail }}
+        </div>
+    </div>
+</div>
+@stop
